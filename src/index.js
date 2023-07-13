c618bc56690b592af9043b9eba00969c66930c05
chaban-dmytro <dimach@ukr.net>
2023-07-13 13:41:17 +0200

    almoust finish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { fetchBreeds } from "./cat-api";
 import { fetchCatByBreed } from "./cat-api";
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+new SlimSelect({
+  select: '#slim-select'
+})
 
 const selectInputEl = document.querySelector( '.breed-select' );
 const catItemEl = document.querySelector( '.cat-info' );
+const loaderEl = document.querySelector( '.loader' );
+const errorEl = document.querySelector( '.error' );
 
 selectInputEl.addEventListener( 'input', checkInput );
 
@@ -10,9 +18,9 @@ let allCatsArray = {};
 
 fetchBreeds().then( data => {
   allCatsArray = data;
-  console.log( allCatsArray );
   getValuesToInput( allCatsArray );
-} ).catch( err => console.log( err ) );
+  loaderEl.classList.add( 'hidden' );
+} ).catch( err => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!') );
 
 function getValuesToInput( array ) {
   let markup = [];
@@ -23,18 +31,21 @@ function getValuesToInput( array ) {
 }
 
 function checkInput( event ) {
+  loaderEl.classList.remove( 'hidden' );
   fetchCatByBreed(event.target.value)
     .then( data => {
       let dataInfo = data[0].breeds[0];
-      console.log( dataInfo );
-      catItemEl.innerHTML = `<img src=${data[0].url} alt=${dataInfo.name} width=500/>
-      <h2>${dataInfo.name}</h2>
-      <p>${dataInfo.description}</p>
-      <p>
-        <span>Temperament: </span>
-        ${dataInfo.temperament}
-      </p>`;
-} ).catch( err => console.log( err ) );
+      catItemEl.innerHTML = `<img class="cat-img" src=${data[0].url} alt=${dataInfo.name} width=500/>
+      <div class="info-wrapper">
+        <h2>${dataInfo.name}</h2>
+        <p>${dataInfo.description}</p>
+          <p>
+            <span>Temperament:</span>
+            ${dataInfo.temperament}
+          </p>
+        </div>`;      
+      loaderEl.classList.add( 'hidden' );
+} ).catch( err => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!') );
 }
 
 
