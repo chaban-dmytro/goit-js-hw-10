75b3bcbbac76c40f24658ba90ad2596a56196b7b
chaban-dmytro <dimach@ukr.net>
2023-07-14 13:42:17 +0200

    add slim-select

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import { fetchBreeds } from "./cat-api";
 import { fetchCatByBreed } from "./cat-api";
 import SlimSelect from 'slim-select';
+import "slim-select/dist/slimselect.css";
 import Notiflix from 'notiflix';
-
-new SlimSelect({
-  select: '#slim-select'
-})
 
 const selectInputEl = document.querySelector( '.breed-select' );
 const catItemEl = document.querySelector( '.cat-info' );
 const loaderEl = document.querySelector( '.loader' );
-const errorEl = document.querySelector( '.error' );
 
 selectInputEl.addEventListener( 'input', checkInput );
 
@@ -28,6 +24,9 @@ function getValuesToInput( array ) {
     markup.push(`<option value=${element.id}>${element.name}</option>`)
   } );
   selectInputEl.innerHTML = markup.join( ' ' );
+  // new SlimSelect({
+  //   select: selectInputEl
+  // } )
 }
 
 function checkInput( event ) {
@@ -35,18 +34,23 @@ function checkInput( event ) {
   fetchCatByBreed(event.target.value)
     .then( data => {
       let dataInfo = data[0].breeds[0];
-      catItemEl.innerHTML = `<img class="cat-img" src=${data[0].url} alt=${dataInfo.name} width=500/>
-      <div class="info-wrapper">
-        <h2>${dataInfo.name}</h2>
-        <p>${dataInfo.description}</p>
-          <p>
-            <span>Temperament:</span>
-            ${dataInfo.temperament}
-          </p>
-        </div>`;      
+      createCatMarkup( data, dataInfo );     
       loaderEl.classList.add( 'hidden' );
 } ).catch( err => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!') );
 }
+
+function createCatMarkup(data, dataInfo) {
+  catItemEl.innerHTML = `<img class="cat-img" src=${data[0].url} alt=${dataInfo.name} width=500/>
+  <div class="info-wrapper">
+    <h2>${dataInfo.name}</h2>
+    <p>${dataInfo.description}</p>
+      <p>
+        <span>Temperament:</span>
+        ${dataInfo.temperament}
+      </p>
+    </div>`;      
+}
+
 
 
 
